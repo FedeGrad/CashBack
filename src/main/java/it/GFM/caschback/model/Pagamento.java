75873fdd9b8e509194c4f0526ef94e61ee50c995
Federico Gradizzi <101737604+FedeGrad@users.noreply.github.com>
2022-06-02 15:32:44 +0200

    progetto

---
+++ src/main/java/it/GFM/caschback/model/Pagamento.java
@@ -0,0 +1,15 @@
+package it.GFM.caschback.model;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pagamento {
+
+}
---
+++ src/test/java/it/GFM/caschback/CashBackApplicationTests.java
@@ -0,0 +1,13 @@
+package it.GFM.caschback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
