5f817532b9d47af0c2807fc5749bac92be53434e
Frens13 <donatifrancesco13@gmail.com>
2022-06-02 17:04:15 +0200

    commit Preferenza

--- src/main/java/it/GFM/caschback/model/Preferenza.java
+++ src/main/java/it/GFM/caschback/model/Preferenza.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Preferenza {
-	//ok4
+	//ok5
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
