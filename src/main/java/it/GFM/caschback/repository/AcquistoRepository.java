efac090692b151d4ec62641607a4d716b8164b67
Federico Gradizzi <101737604+FedeGrad@users.noreply.github.com>
2022-06-03 00:35:53 +0200

    acquisto repo

--- src/main/java/it/GFM/caschback/repository/AcquistoRepository.java
+++ src/main/java/it/GFM/caschback/repository/AcquistoRepository.java
@@ -1,9 +1,20 @@
 package it.GFM.caschback.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import it.GFM.caschback.model.Acquisto;
+import it.GFM.caschback.model.Offerta;
+import it.GFM.caschback.model.Utente;
 
 public interface AcquistoRepository extends JpaRepository<Acquisto, Long> {
-
+	
+	@Query(value = "", nativeQuery = true)
+	public List<Acquisto> findByUtente(Utente utente);
+	public List<Acquisto> findByDataAcquisto(LocalDate dataAcquisto);
+	public List<Acquisto> findByOfferta(List<Offerta> offerta);
+	
 }
