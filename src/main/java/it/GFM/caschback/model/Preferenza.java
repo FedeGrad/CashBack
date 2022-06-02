cdab203bdf04c6456bd531bc3298a357a6d92b6c
Frens13 <donatifrancesco13@gmail.com>
2022-06-02 16:55:59 +0200

    commit Francesco Entity Preferenza

--- src/main/java/it/GFM/caschback/model/Preferenza.java
+++ src/main/java/it/GFM/caschback/model/Preferenza.java
@@ -1,6 +1,15 @@
 package it.GFM.caschback.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,5 +20,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Preferenza {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private List<String> username = new ArrayList<String>();
+	private List<String> viaggi = new ArrayList<String>();
+	private List<String> casa = new ArrayList<String>();
+	private List<String> libri = new ArrayList<String>();
+	private List<String> cosmetici = new ArrayList<String>();
+	private List<String> ristoranti = new ArrayList<String>();
+	private List<String> alberghi = new ArrayList<String>();
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "username_utente")
+	private Utente username_utente;
 }
