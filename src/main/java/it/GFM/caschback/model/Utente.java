d615954da1a65ebff4acf0602fe663828335d0a3
Federico Gradizzi <101737604+FedeGrad@users.noreply.github.com>
2022-06-02 16:44:12 +0200

    utente e profilo

--- src/main/java/it/GFM/caschback/model/Utente.java
+++ src/main/java/it/GFM/caschback/model/Utente.java
@@ -1,10 +1,20 @@
 package it.GFM.caschback.model;
 
-import javax.persistence.Entity;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Data
@@ -12,6 +22,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Utente {
 	
-	private String nome;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Setter(value = AccessLevel.NONE)
+	long id;
+	private String username;
+	private String password;
+	@OneToMany(mappedBy = "utente", cascade = CascadeType.ALL)
+	private List<Acquisto> acquisti = new ArrayList<Acquisto>();
+	@OneToMany(mappedBy = "utente", cascade = CascadeType.ALL)
+	private List<Profilo> profili = new ArrayList<Profilo>();
+	@OneToMany(mappedBy = "utente", cascade = CascadeType.ALL)
+	private List<Pagamento> pagamenti = new ArrayList<Pagamento>();
 
 }
