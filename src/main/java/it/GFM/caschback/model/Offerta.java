0923e4e9203a4f412ad72698245d9e5d4e775fc3
Giovanni <Giovanni@DESKTOP-Q1Q9MQL.homenet.telecomitalia.it>
2022-06-02 17:03:43 +0200

    prova commit

--- src/main/java/it/GFM/caschback/model/Offerta.java
+++ src/main/java/it/GFM/caschback/model/Offerta.java
@@ -19,7 +19,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Offerta {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
